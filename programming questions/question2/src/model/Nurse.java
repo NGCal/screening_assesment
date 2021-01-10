2873d55f993dbcb2f3193277f7c68346f79ef860
Nathalia G <r4ng@localhost.localdomain>
2021-01-09 23:05:45 -0500

    Created nurse class with methods to treat patient

---
+++ programming questions/question2/src/model/Nurse.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Nurse extends Person{
+
+    public String askto_OpenMouth(Person patient){
+        String ans = patient.change_mouthStatus("open");
+        return ans;
+
+    }
+
+    public String askto_OpenMouth(Man patient){
+        String ans = patient.change_MouthStatus("open", this);
+        return ans;
+    }
+
+    public String askto_CloseMouth(Person patient){
+        String ans = patient.change_mouthStatus("close");
+        return ans;
+    }
+
+    public String askto_CloseMouth(Man patient){
+        String ans = patient.change_MouthStatus("close", this);
+        return ans;
+    }
+
+
+    
+}
